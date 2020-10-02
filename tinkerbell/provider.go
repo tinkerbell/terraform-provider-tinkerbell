697d3bd3d092a6879ff435004ac507691d7f3137
Mateusz Gozdek <mateusz@kinvolk.io>
2020-10-02 18:06:54 +0200

    Add grpc_authority and cert_url provider configuration fields
    So users are not forced to use environment variables to configure the
    provider.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- tinkerbell/provider.go
+++ tinkerbell/provider.go
@@ -2,6 +2,7 @@ package tinkerbell
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
@@ -20,6 +21,18 @@ type tinkClient struct {
 // Provider returns the Tinkerbell terraform provider.
 func Provider() *schema.Provider {
 	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"grpc_authority": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Equivalent of TINKERBELL_GRPC_AUTHORITY environment variable.",
+			},
+			"cert_url": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Equivalent of TINKERBELL_CERT_URL environment variable.",
+			},
+		},
 		ResourcesMap: map[string]*schema.Resource{
 			"tinkerbell_template": resourceTemplate(),
 			"tinkerbell_workflow": resourceWorkflow(),
@@ -30,9 +43,21 @@ func Provider() *schema.Provider {
 }
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+	if v := d.Get("grpc_authority").(string); v != "" {
+		if err := os.Setenv("TINKERBELL_GRPC_AUTHORITY", v); err != nil {
+			return nil, fmt.Errorf("setting TINKERBELL_GRPC_AUTHORITY environment variable: %w", err)
+		}
+	}
+
+	if v := d.Get("cert_url").(string); v != "" {
+		if err := os.Setenv("TINKERBELL_CERT_URL", v); err != nil {
+			return nil, fmt.Errorf("setting TINKERBELL_CERT_URL environment variable: %w", err)
+		}
+	}
+
 	conn, err := client.GetConnection()
 	if err != nil {
-		return nil, fmt.Errorf("failed to create tink client: %w", err)
+		return nil, fmt.Errorf("creating tink client: %w", err)
 	}
 
 	return &tinkClient{
